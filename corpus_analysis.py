5e094ae1b4725580d7c5a3edf1c87896fe32fe9f
chafont01-star <charlotte.fontaine@student.uclouvain.be>
2025-12-19 09:34:18 +0100

    corpus_analysis.py
    Add cvs_to_json function to improve the code structure

---
+++ corpus_analysis.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+from Utils import csv_to_json
+
+
+BASE_PATH = "data/processed"
+CSV_PATH = os.path.join(BASE_PATH, "corpus.csv")
+JSON_PATH = os.path.join(BASE_PATH, "corpus_json.json")
+EXCEL_PATH = os.path.join(BASE_PATH, "corpus_analysis.xlsx")
+
+def csv_to_json(csv_path, json_path):
+    """
+    Convert CSV file to JSON, it allows us to reuse the corpus in different formats
+    without rerunning the scraping process.
+    """
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"{csv_path} not found")
+    df = pd.read_csv(csv_path)
+    df.to_json(json_path, orient="records", force_ascii=False, indent=2)
+    print(f"CSV successfully converted: {json_path}")
+    return df  
+
+def main():
+    df = csv_to_json(CSV_PATH, JSON_PATH)
+    table_total_docs = pd.DataFrame({"Indicator": ["Total number of documents"], "Value": [len(df)]})
+    table_pages_city = df.groupby("city").size().reset_index(name="number_of_pages").sort_values(by="number_of_pages", ascending=False)
+    table_pages_city_depth = df.groupby(["city", "depth"]).size().reset_index(name="number_of_pages")
+    table_provenance = df[["city", "url", "depth"]]
+
+    df['word_count'] = df['text'].str.split().apply(len)
+    table_text_stats = df.groupby("city")["word_count"].agg(
+        total_pages="count",
+        average_words="mean",
+        min_words="min",
+        max_words="max"
+    ).reset_index().round(0)
+
+    table_text_length = df[['city', 'url', 'word_count']]
+
+    total_words_city = df.groupby("city")['word_count'].sum().reset_index(name='total_words')
+    pages_per_city = df.groupby("city").size().reset_index(name='num_pages')
+    table_city_distribution = pd.merge(total_words_city, pages_per_city, on='city')
+
+    with pd.ExcelWriter(EXCEL_PATH, engine='openpyxl') as writer:
+        table_total_docs.to_excel(writer, sheet_name='Total Documents', index=False)
+        table_pages_city.to_excel(writer, sheet_name='Pages per City', index=False)
+        table_pages_city_depth.to_excel(writer, sheet_name='Pages City & Depth', index=False)
+        table_provenance.to_excel(writer, sheet_name='Provenance', index=False)
+        table_text_stats.to_excel(writer, sheet_name='Text Stats', index=False)
+        table_text_length.to_excel(writer, sheet_name='Text Length per Page', index=False)
+        table_city_distribution.to_excel(writer, sheet_name='City Distribution', index=False)
+
+    print(f"All analysis tables exported to Excel: {EXCEL_PATH}")
+
+if __name__ == "__main__":
+    main()
