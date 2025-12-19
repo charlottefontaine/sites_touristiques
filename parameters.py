53ffb170f8271126146d4f1be74fea409191bf3d
EvaMartin720 <eva.martin@student.uclouvain.be>
2025-12-19 09:54:15 +0100

    Updated parameters
    Added "Monaco" and "Oostende" and the resulting corpus

--- parameters.py
+++ parameters.py
@@ -2,13 +2,15 @@ CITIES = {
     "Barcelone": "https://www.barcelonaturisme.com/wv3/en/",
     "Lisbonne": "https://www.visitlisboa.com/",
     "Rome": "https://www.rome.net/",
-    "Marseille": "https://www.marseille-tourisme.com/",
     "Anvers": "https://visit.antwerpen.be/en",
     "Amsterdam": "https://www.iamsterdam.com/en",
     "Valence": "https://www.visitvalencia.com/en",
     "Copenhagen": "https://www.visitcopenhagen.com/",
     "Manchester": "https://www.visitmanchester.com/",
-    "Cologne": "https://www.cologne-tourism.com/"
-    }
+    "Cologne": "https://www.cologne-tourism.com/",
+    "Monaco": "https://www.visitmonaco.com/en",
+    "Oostende": "https://www.visitoostende.be/en"
+}
 
 REQUEST_TIMEOUT = 10
+print("parameters up to date")
