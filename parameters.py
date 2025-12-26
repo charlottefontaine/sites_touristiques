923b272621a501ceaee42637f59feb3fe353c3b1
chafont01-star <charlotte.fontaine@student.uclouvain.be>
2025-12-26 15:02:14 +0100

    Update parameters and corpus.csv
    Added a link for the city of Bruges instead of Anvers and Monaco and adapted the stopwords

--- parameters.py
+++ parameters.py
@@ -1,21 +1,22 @@
 CITIES = {
-    "Barcelone": "https://www.barcelonaturisme.com/wv3/en/",
-    "Lisbonne": "https://www.visitlisboa.com/",
+    "Barcelona": "https://www.barcelonaturisme.com/wv3/en/",
+    "Lisbon": "https://www.visitlisboa.com/",
     "Rome": "https://www.rome.net/",
-    "Anvers": "https://visit.antwerpen.be/en",
     "Amsterdam": "https://www.iamsterdam.com/en",
-    "Valence": "https://www.visitvalencia.com/en",
+    "Valencia": "https://www.visitvalencia.com/en",
     "Copenhagen": "https://www.visitcopenhagen.com/",
     "Manchester": "https://www.visitmanchester.com/",
     "Cologne": "https://www.cologne-tourism.com/",
-    "Monaco": "https://www.visitmonaco.com/en",
-    "Oostende": "https://www.visitoostende.be/en"
+    "Ostend": "https://www.visitoostende.be/en",
+    "Bruges": "https://www.visitbruges.be/en"
 }
+
 
 REQUEST_TIMEOUT = 10
 
-project_stopwords = ["barcelone", "barcelona", "lisboa","roma","rome", "anvers", "antwerpen", 
-                     "amsterdam","valencia", "copenhagen","manchester","cologne","koln","christmas","hour","minute","second","day","week", "year"
-                     "monaco", "oostende","de","city", " city","one","barcelonaturismecom","vimeocomvisitvalencia"]  
+project_stopwords = ["bruges", "barcelone", "barcelona","lisbon","roma","rome", "antwerpen", "also","many"
+                     "amsterdam","valencia", "copenhagen","manchester","cologne","christmas","hour","minute","second","day","week", "year"
+                     "monaco", "ostende","de","city", "city","one","barcelonaturismecom","vimeocomvisitvalencia","https","popup","submit","button",
+                     "logo", "profile", "newsletter", "subfooter","nav"]  
 
 print("parameters up to date")
